3597971c51a472c8160f573ea756c14959fb9584
Yahya Jabary <jabaryyahya@gmail.com>
2023-11-05 14:51:56 -0500

    update

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    printWidth: 160,
+    htmlWhitespaceSensitivity: 'ignore',
+    bracketSameLine: true,
+}
